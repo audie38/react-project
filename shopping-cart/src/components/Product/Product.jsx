1773f61da80d3bba97e68de7b6ae9412ae54c00e
Audie Milson <milson.audie@gmail.com>
2023-07-24 16:02:06 +0700

    Add Carousel Component

--- shopping-cart/src/components/Product/Product.jsx
+++ shopping-cart/src/components/Product/Product.jsx
@@ -1,9 +1,11 @@
 import ProductList from "./ProductList";
+import Carousel from "../UI/Carousel";
 import PropTypes from "prop-types";
 
 const Product = (props) => {
   return (
     <div>
+      <Carousel data={props.data} />
       <ProductList products={props.data} />
     </div>
   );
---
+++ shopping-cart/src/components/UI/Carousel.jsx
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+export default function Carousel(props) {
+  const sliderData = props?.data?.slice(1, props.data.length - 1);
+  return (
+    <div id="carouselExample" className="carousel carousel-dark slide">
+      <div className="carousel-inner">
+        <Link to={`/product/${props.data[0].slug}`} className="carousel-item active">
+          <img src={props.data[0].img} className="d-block w-100 object-fit-contain" alt={props.data[0].name} style={{ maxHeight: 300, objectFit: "cover" }} />
+        </Link>
+        {sliderData.map((item) => (
+          <Link to={`/product/${item.slug}`} key={item.id} className="carousel-item ">
+            <img src={item.img} className="d-block w-100 object-fit-contain" alt={item.name} style={{ maxHeight: 300, objectFit: "cover" }} />
+          </Link>
+        ))}
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
+
+Carousel.propTypes = {
+  data: PropTypes.array,
+};
