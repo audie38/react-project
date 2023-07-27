359926541e0ce264ecc4068aaf405c1d5c309500
Audie Milson <milson.audie@gmail.com>
2023-07-27 20:13:42 +0700

    Modify Fetch API using Async Await

--- react-movies/src/App.jsx
+++ react-movies/src/App.jsx
@@ -5,32 +5,24 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films/", {
+  const fetchMoviesHandler = async () => {
+    const result = await fetch("https://swapi.dev/api/films/", {
       method: "GET",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error();
-        }
-        return res.json();
-      })
-      .then((data) => {
-        const transformedData = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedData);
-      })
-      .catch((err) => console.log(err));
+    });
+    const data = await result.json();
+    const transformedData = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedData);
   };
 
   return (
