c7e05ac0270a9277db77422b55d4c45ae4dcbb4d
Audie Milson <milson.audie@gmail.com>
2023-08-01 22:10:12 +0700

    Update Redux State and Send Request to Server

--- react-redux-shopping-cart/src/App.jsx
+++ react-redux-shopping-cart/src/App.jsx
@@ -1,14 +1,63 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import Layout from "./components/Layout/Layout";
 import Cart from "./components/Cart/Cart";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { productActions } from "./store/product";
+
+let initialState = true;
 
 export default function App() {
   const showCart = useSelector((state) => state.product.showCart);
   const notification = useSelector((state) => state.product.notification);
+  const cart = useSelector((state) => state.product.cart);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const sendRequest = async () => {
+      dispatch(
+        productActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending the Data",
+        })
+      );
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/cartItem`, {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cart),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to Send data");
+      }
+      dispatch(
+        productActions.showNotification({
+          status: "success",
+          title: "Sending Data Success",
+          message: "Success Sending the Data",
+        })
+      );
+    };
+    if (initialState) {
+      initialState = false;
+      return;
+    }
+    sendRequest().catch((err) => {
+      dispatch(
+        productActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: err,
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
     <Fragment>
