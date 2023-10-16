1831fa3ce1100d93939ce663ad761a78de850759
Audie Milson <milson.audie@gmail.com>
2023-10-16 18:50:43 +0700

    Add Navbar Component

---
+++ react-notes/src/components/ui/NavigationBar.tsx
@@ -0,0 +1,21 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavigationBar() {
+  return (
+    <Navbar expand="lg" className="navbar-dark bg-dark">
+      <Container>
+        <Navbar.Brand href="/">React-Notes</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/note">Note</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
